49b8c26437861fbb1fe8f1b0d93b22261beb64a1
ychen563 <63408773+ychen563@users.noreply.github.com>
2020-06-03 01:05:53 +0800

    test1

--- Lab11/source/main.c
+++ Lab11/source/main.c
@@ -11,13 +11,37 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+#include "keypad.h"
+
+unsigned char 
 
 int main(void) {
+    unsigned char x;
     /* Insert DDR and PORT initializations */
-
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0xF0; PORTC = 0x0F;
     /* Insert your solution below */
     while (1) {
-
+    x = GetKeypadKey();
+        switch(x){
+            case '\0':PORTB = 0x1F; break;
+            case '1' :PORTB = 0x01; break;
+            case '2' :PORTB = 0x02; break;
+            case '3' :PORTB = 0x03; break;
+            case '4' :PORTB = 0x04; break;
+            case '5' :PORTB = 0x05; break;
+            case '6' :PORTB = 0x06; break;
+            case '7' :PORTB = 0x07; break;
+            case '8' :PORTB = 0x08; break;
+            case '9' :PORTB = 0x09; break;
+            case 'A' :PORTB = 0x0A; break;
+            case 'B' :PORTB = 0x0B; break;
+            case 'C' :PORTB = 0x0C; break;
+            case 'D' :PORTB = 0x0D; break;
+            case '*' :PORTB = 0x0E; break;
+            case '0' :PORTB = 0x00; break;
+            case '#' :PORTB = 0x0F; break;
+            default: PORTB = 0x1B; break;
+        }
     }
-    return 1;
 }
