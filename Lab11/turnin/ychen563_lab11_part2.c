b6c8ff300c928ee1a3a7de3cf108feca4b2299f8
Ubuntu <ychen563@ucr.edu>
2020-06-06 12:08:23 -0700

    part3 done

--- Lab11/turnin/ychen563_lab11_part2.c
+++ Lab11/turnin/ychen563_lab11_part2.c
@@ -1,6 +1,6 @@
 /*	Author: ychen563
  *	Lab Section:024
- *	Assignment: Lab #11  Exercise #3
+ *	Assignment: Lab #11  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -15,46 +15,15 @@
 #include "io.h"
 #endif
 
-unsigned char input = ' ';
+unsigned char message[] = " CS120B is Legend... wait for it DARY!     ";
 
-enum keypad_States {keypad_display};
-
-int keypadSMTick(int state) {
-    
-    unsigned char x = GetKeypadKey();
-    switch (state) {
-        case keypad_display: state = keypad_display; break;
-        default: state = keypad_display; break;
-    }
-    switch (state) {
-        case keypad_display:
-            switch(x){
-            case '1' :input = '1'; break;
-            case '2' :input = '2'; break;
-            case '3' :input = '3'; break;
-            case '4' :input = '4'; break;
-            case '5' :input = '5'; break;
-            case '6' :input = '6'; break;
-            case '7' :input = '7'; break;
-            case '8' :input = '8'; break;
-            case '9' :input = '9'; break;
-            case 'A' :input = 'A'; break;
-            case 'B' :input = 'B'; break;
-            case 'C' :input = 'C'; break;
-            case 'D' :input = 'D'; break;
-            case '*' :input = '*'; break;
-            case '0' :input = '0'; break;
-            case '#' :input = '#'; break;
-            }
-        break;
-       default: break;
-    }
-    return state;
-}
 
 enum LCD_display_states {LCD_display};
 
 int LCDSMTick(int state){
+	
+    unsigned char display[16] ="";
+    unsigned short i;
     
     switch(state){
         case LCD_display: state = LCD_display; break;
@@ -62,11 +31,19 @@ int LCDSMTick(int state){
     }
     switch(state){
         case LCD_display:
-            LCD_ClearScreen();
-            LCD_WriteData(input);
+            for (i = 0; i < ((sizeof(message)/sizeof(message[0]))-2); i++){
+                message[i] = message[i+1];
+            }
+            message[(sizeof(message)/sizeof(message[0]))-2] = message[0];
+            
+            for (i = 0; i < ((sizeof(display)/sizeof(display[0]))-1); i++){
+                display[i] = message[i];
+            }
+            display[(sizeof(display)/sizeof(display[0]))-1] = '\0';
             break;
         default: break;
     }
+    LCD_DisplayString(1, display);
     return state;
 }
 
@@ -77,19 +54,14 @@ int main() {
     DDRB = 0xFF; PORTB = 0x00;
     DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
-    static task task1, task2;
-    task *tasks[] = {&task1, &task2};
+    static task task2;
+    task *tasks[] = {&task2};
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
     const char start = -1;
 
-    task1.state = start;
-    task1.period = 50;
-    task1.elapsedTime  = task1.period;
-    task1.TickFct = &keypadSMTick;
-
     task2.state = start;
-    task2.period = 100;
+    task2.period = 300;
     task2.elapsedTime  = task2.period;
     task2.TickFct = &LCDSMTick;
 
